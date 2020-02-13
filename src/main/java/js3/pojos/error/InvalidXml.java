7b0366f296ecbba094dc43e619aa5e8c91047449
Jurgen Voorneveld <jurgen@ngti.nl>
2020-02-13 17:09:30 +0100

    Removed unused exception

--- src/main/java/js3/pojos/error/InvalidXml.java
+++ src/main/java/js3/pojos/error/InvalidXml.java
@@ -3,7 +3,6 @@ package js3.pojos.error;
 import java.io.IOException;
 
 public final class InvalidXml extends IOException {
-    public InvalidXml() {}
     public InvalidXml(final String message) {
         super(message);
     }
